846b344dc127e46f386bacde13df20c99fa98622
JavaResources <mahamudul.duet87@gmail.com>
2020-06-03 10:53:17 +0900

    golang-client-server-socket-starter

---
+++ client.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"log"
+	"net"
+	"os"
+	"strconv"
+	"strings"
+)
+
+const (
+	message       = "Ping"
+	StopCharacter = "\r\n\r\n"
+)
+
+func SocketClient(ip string, port int) {
+	addr := strings.Join([]string{ip, strconv.Itoa(port)}, ":")
+	conn, err := net.Dial("tcp", addr)
+
+	if err != nil {
+		log.Fatalln(err)
+		os.Exit(1)
+	}
+
+	defer conn.Close()
+
+	conn.Write([]byte(message))
+	conn.Write([]byte(StopCharacter))
+	log.Printf("Send: %s", message)
+
+	buff := make([]byte, 1024)
+	n, _ := conn.Read(buff)
+	log.Printf("Receive: %s", buff[:n])
+
+}
+
+func main() {
+
+	var (
+		ip   = "127.0.0.1"
+		port = 3333
+	)
+
+	SocketClient(ip, port)
+
+}
